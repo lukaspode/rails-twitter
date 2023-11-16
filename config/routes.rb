a62c3b28433cba6812023f347cd51aadfdedc9ea
lucas podesta <lukitaspo@hotmail.com>
2023-11-16 15:44:51 -0300

    view SignUp created

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions' # ,
-    # registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'#,
     # passwords: 'users/passwords'
   }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
