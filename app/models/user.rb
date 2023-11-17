29d846202e22a262f997868217927d7674f598e4
lucas podesta <lukitaspo@hotmail.com>
2023-11-17 14:16:53 -0300

    validate age over 18

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, :birth, presence: true
+  validate :validate_age
+
+  private
+
+  def validate_age
+    return unless birth.present? && (birth.to_date > 18.years.ago.to_date)
+
+    errors.add(:birth, ': your age must be over 18')
+  end
 end
