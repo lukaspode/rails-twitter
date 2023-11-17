ade3f2ec54bd64731493232922aa8bf45a3e30b5
lucas podesta <lukitaspo@hotmail.com>
2023-11-17 12:28:34 -0300

    'rubocop' no errors

--- db/migrate/20231117134407_add_fields_to_users.rb
+++ db/migrate/20231117134407_add_fields_to_users.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class AddFieldsToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :birth, :date
--- db/migrate/20231117140549_add_name_to_users.rb
+++ db/migrate/20231117140549_add_name_to_users.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class AddNameToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :name, :string
