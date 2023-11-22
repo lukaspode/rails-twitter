c8054573072d787a2a689ef0c81072d4b2d345c7
lucas podesta <lukitaspo@hotmail.com>
2023-11-22 12:20:41 -0300

    changes on migration

--- db/migrate/20231121184619_add_confirmable_to_devise.rb
+++ db/migrate/20231121184619_add_confirmable_to_devise.rb
@@ -1,22 +1,13 @@
 # frozen_string_literal: true
 
 class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
-  def up
+  def change
     change_table :users, bulk: true do |t|
       t.string :confirmation_token
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string :unconfirmed_email
       t.index :confirmation_token, unique: true
-    end
-  end
-
-  def down
-    change_table :users, bulk: true do |t|
-      t.remove :confirmation_token
-      t.remove :confirmed_at
-      t.remove :confirmation_sent_at
-      t.remove :unconfirmed_email
     end
   end
 end
