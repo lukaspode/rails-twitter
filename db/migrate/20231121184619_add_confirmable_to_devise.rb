3361434e20caeb210ab453453a7863ce804d1e8d
lukaspode <68868902+lukaspode@users.noreply.github.com>
2023-11-22 14:52:59 -0300

    Email confirmation (#13)
    * email confirmation with 'Confirmable'
    
    * rubocop errors
    
    * changes on migration
    
    * confirmatiomn_token index change
    
    * rubocop errors corrected

---
+++ db/migrate/20231121184619_add_confirmable_to_devise.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
+  def change
+    change_table :users, bulk: true do |t|
+      t.string :confirmation_token, index: { unique: true }
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string :unconfirmed_email
+    end
+  end
+end
