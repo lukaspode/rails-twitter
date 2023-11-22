b197d1739e89f3513bdd4d50b2a3902fa1939431
lucas podesta <lukitaspo@hotmail.com>
2023-11-22 14:49:39 -0300

    rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,17 +7,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
 
-  validates :name, length: { minimum: 2 } , presence: true
+  validates :name, length: { minimum: 2 }, presence: true
   validates :birth, presence: true
-  validates :username, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 20}, presence: true
+  validates :username, uniqueness: { case_sensitive: false }, length: { minimum: 2, maximum: 20 }, presence: true
   validate :validate_age
-  validates :bio, length: { maximum: 160}
-  validates :website, format: { with: /http(s)?:\/\/([a-z0-9-]+\.)?[a-z0-9-]+(\.[a-z]{2,})+/ }, allow_nil: true
+  validates :bio, length: { maximum: 160 }
+  validates :website, format: { with: %r{http(s)?://([a-z0-9-]+\.)?[a-z0-9-]+(\.[a-z]{2,})+} }, allow_nil: true
 
   private
 
   def validate_age
     return unless birth.present? && (birth > 18.years.ago.to_date)
+
     errors.add(:birth, 'your age must be over 18')
   end
 end
--- db/migrate/20231122161156_add_new_fields_to_user.rb
+++ db/migrate/20231122161156_add_new_fields_to_user.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 class AddNewFieldsToUser < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :bio, :text
-    add_column :users, :website, :string
+    change_table :users, bulk: true do |t|
+      t.text :bio
+      t.string :website
+    end
   end
 end
