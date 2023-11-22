389c1b39485c9b9f6eab6e671cb56a0746c9dbfa
lucas podesta <lukitaspo@hotmail.com>
2023-11-22 14:44:29 -0300

    rubocop errors corrected

--- db/migrate/20231121184619_add_confirmable_to_devise.rb
+++ db/migrate/20231121184619_add_confirmable_to_devise.rb
@@ -3,7 +3,7 @@
 class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
   def change
     change_table :users, bulk: true do |t|
-      t.string :confirmation_token, index: {unique: true}
+      t.string :confirmation_token, index: { unique: true }
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string :unconfirmed_email
