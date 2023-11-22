cf7bd45facf49265bb9a3582f59f28f6fbb88433
lucas podesta <lukitaspo@hotmail.com>
2023-11-22 14:42:15 -0300

    confirmatiomn_token index change

--- db/migrate/20231121184619_add_confirmable_to_devise.rb
+++ db/migrate/20231121184619_add_confirmable_to_devise.rb
@@ -3,11 +3,10 @@
 class AddConfirmableToDevise < ActiveRecord::Migration[7.1]
   def change
     change_table :users, bulk: true do |t|
-      t.string :confirmation_token
+      t.string :confirmation_token, index: {unique: true}
       t.datetime :confirmed_at
       t.datetime :confirmation_sent_at
       t.string :unconfirmed_email
-      t.index :confirmation_token, unique: true
     end
   end
 end
