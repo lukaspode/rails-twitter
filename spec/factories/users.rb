3e3ec1fe5ef90755bc8d164d1e6c07793ef0b9e2
lucas podesta <lukitaspo@hotmail.com>
2023-11-24 12:38:26 -0300

    comment deleted

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,7 +9,7 @@ FactoryBot.define do
     sequence :username do |n|
       Faker::Internet.username.to_s + n.to_s
     end
-    bio { Faker::Lorem.paragraph(sentence_count: 0) }
-    website { Faker::Lorem.paragraph(sentence_count: 0) }
+    bio { Faker::Lorem.paragraph_by_chars(number: Faker::Number.between(from: 1, to: 160)) }
+    website { Faker::Internet.url }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,8 +4,6 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    # subject { user.valid? }
-
     subject(:user) { build(:user) }
 
     it { is_expected.to validate_presence_of(:name) }
