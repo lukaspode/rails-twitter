b0f429665dc33b9a6937352cb12b262275d783bf
lucas podesta <lukitaspo@hotmail.com>
2023-11-28 13:26:55 -0300

    edit button only displayed for current user logged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  helper_method :current_user?
 end
