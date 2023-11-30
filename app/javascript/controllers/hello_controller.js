ec7763e4bd069cf57dc074aaa8e910b8613b38d7
lucas podesta <lukitaspo@hotmail.com>
2023-11-30 10:34:01 -0300

    edit profile save button

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,11 +1,11 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() { }
-  close(e){
-    e.preventDefault()
-    const modal = document.getElementById('modal')
+  // connect() { }
+  // close(e){
+  //   e.preventDefault()
+  //   const modal = document.getElementById('modal')
 
-    modal.innerHTML = " "
-  }
+  //   modal.innerHTML = " "
+  // }
 }
