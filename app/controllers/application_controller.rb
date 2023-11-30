abcf0161d891f731d5c52d94cb4f37f7658143f4
lucas podesta <lukitaspo@hotmail.com>
2023-11-30 16:07:04 -0300

    authenticate_user devise added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :current_user?
-
-  def current_user?(user)
-    user == current_user
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: %i[edit update]
   before_action :set_user, only: %i[show edit update]
-  before_action :require_signin, only: %i[edit update]
   before_action :user_equal_current_user?, only: %i[edit update]
+
+  helper_method :current_user?
 
   def show; end
 
@@ -19,18 +21,16 @@ class UsersController < ApplicationController
 
   private
 
+  def current_user?(user)
+    user == current_user
+  end
+
   def user_params
     params.require(:user).permit(:name, :bio, :website, :birth)
   end
 
-  def require_signin
-    return if current_user
-
-    redirect_to new_user_session_path
-  end
-
   def user_equal_current_user?
-    return false if current_user?(@user) && current_user
+    return false if current_user?(@user)
 
     redirect_to user_path(@user)
   end
