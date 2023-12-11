73186b82573a189748ea981818be77cd39dca9b4
lucas podesta <lukitaspo@hotmail.com>
2023-12-11 14:18:12 -0300

    tweet_count field migration created

---
+++ db/migrate/20231211163021_add_tweet_count_to_user.rb
@@ -0,0 +1,20 @@
+class AddTweetCountToUser < ActiveRecord::Migration[7.1]
+  def up
+    change_table :users, bulk: true do |t|
+      t.integer :tweets_count, default: 0, null: false
+    end
+
+    execute <<~SQL.squish
+      UPDATE users u
+      SET tweets_count = (SELECT COUNT(*)
+        FROM tweets t
+        WHERE u.id = t.user_id)
+    SQL
+  end
+
+  def down
+    change_table :users, bulk: true do |t|
+      t.remove :tweets_count
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_07_154927) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_11_163021) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,6 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_07_154927) do
     t.string "unconfirmed_email"
     t.text "bio"
     t.string "website"
+    t.integer "tweets_count", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
