452311e185c2459484f14ad7671d1393293b80e8
lucas podesta <lukitaspo@hotmail.com>
2023-12-18 11:54:34 -0300

    new/create tweet route changed

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,9 @@ Rails.application.routes.draw do
   }
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  resources :users, only: :show do
-    resources :tweets, only: %i[new create]
-  end
+  resources :users, only: :show
+
+  resources :tweets, only: %i[new create]
+
   resource :user, only: %i[edit update], controller: :user
 end
