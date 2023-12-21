f1a1107dabd5be61b8d1a21c587f2373fb8206e9
lucas podesta <lukitaspo@hotmail.com>
2023-12-21 12:05:57 -0300

    feed showing correctly tweets from users 'i follow'

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -5,7 +5,21 @@ class UserController < ApplicationController
   before_action :user
 
   def index
-    @tweets = Tweet.select('tweets.*').order(created_at: :desc)
+    join_stmt = <<-SQL.squish
+                      INNER JOIN follows AS current_user_follows
+                      ON (current_user_follows.followed_id = tweets.user_id
+                      AND current_user_follows.follower_id = #{current_user.id})
+
+                      LEFT JOIN likes AS current_user_likes
+                      ON current_user_likes.tweet_id = tweets.id
+                      AND current_user_likes.user_id = #{current_user.id}
+    SQL
+
+    @tweets = Tweet.select('tweets.*',
+                           "COUNT(CASE WHEN current_user_likes.user_id = #{current_user.id} THEN 1 ELSE NULL END) AS liked")
+                   .joins(join_stmt)
+                   .order('created_at DESC').group('tweets.id')
+    @tweet = Tweet.new
   end
 
   def edit; end
